f42501a88f81077284cf13fc1e330a30a92cebbe
Haniwa828 <99132172+Haniwa828@users.noreply.github.com>
2022-09-07 23:04:48 +0800

    更新間隔短縮

--- script.js
+++ script.js
@@ -8,14 +8,14 @@
         // date.setMinutes(Number(1));
         // console.log(date)
  
-        // 1秒毎の更新
+        // 0.5秒毎の更新
         setInterval(()=> {
             const date = new Date();
             secondHand.moveHand(date.getSeconds());
             minuteHand.moveHand(date.getMinutes());
             hourHand.moveHand((date.getHours()%12) * 60 + date.getMinutes(), date.getHours());
             // hourHand.moveHand((date.getHours()%12) * 60 + date.getMinutes());
-        }, 1000);
+        }, 500);
     });
 })();
  
